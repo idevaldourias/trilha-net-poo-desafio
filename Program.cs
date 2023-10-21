9b53462511689ca612968037b9320200090efb5a
Idevaldo Urias <idevaldorodrigues@gmail.com>
2023-10-21 18:45:08 -0300

    Implementações realizadas na atividade proposta

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+
+Smartphone nokia = new Nokia("9999999999", "Modelo 1", "111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone IPhone");
+
+Smartphone iphone = new Iphone("44444444", "Modelo 2", "5555555", 120);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
